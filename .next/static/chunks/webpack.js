5e50fa835af866d12357c9e6b77907a749f5a50a
Sett Teta <stetanonsakul@gmail.com>
2023-02-10 14:28:22 +0700

    reverting bacK

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9c2e3e25aa272bd6"; }
+/******/ 		__webpack_require__.h = function() { return "2a755b9e045dd0e5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,16 @@
-import * as React from 'react'
-import VideoCard from './Card.js'
 import Head from 'next/head'
+import Link from 'next/link'
 
-const linkArray = require('./data.json')
 
-function Home() {
-
-  let videoCards = [];
-  for (let i = 0; i < linkArray.length; i++) {
-    let linkVideo = linkArray[i];
-    videoCards.push(<VideoCard link={linkVideo.link} desc={linkVideo.desc} />);
-  }
-
+export default function AboutPage() {
   return (
-    <main role="main">
-
-      <div>
-        <Head>
-          <title>My Next.js App</title>
-          <meta name="description" content="A simple Next.js application" />
-        </Head>
-      </div>
-
-      <section className="jumbotron text-center">
-        <div class="container">
-          <h1 class="jumbotron-heading">Album example</h1>
-          <p class="lead text-muted">
-            Something short and lke it short and sweet, but not too short so folks don't simply skip over it entirely.
-          </p>
-          <p>
-            <a href="#" class="btn btn-primary my-2">Main call to action</a>
-            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-          </p>
-        </div>
-      </section>
-
-      <div class="album py-5 bg-light">
-        <div class="container">
-          <div class="row">
-
-            {videoCards}
-          </div>
-        </div>
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>Index Page</title>
+      </Head>
+      <h1>Sett Teta</h1>
+      <p>Welcome</p>
+      <Link href="/about">About</Link>
+    </>
   )
 }
-
-export default Home
