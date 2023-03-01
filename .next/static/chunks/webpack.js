7f655e0d3a23b1f39a57d4ccdd50a2fa0cb79a26
Sett Teta <stetanonsakul@gmail.com>
2023-03-01 15:22:59 +0700

    2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1fe4ab073da6f74c"; }
+/******/ 		__webpack_require__.h = function() { return "d18cb41311baa639"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
