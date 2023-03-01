063b6b956cbf4fb183f96683a9617849e7aabd3e
Sett Teta <stetanonsakul@gmail.com>
2023-03-01 15:28:22 +0700

    change url 2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d18cb41311baa639"; }
+/******/ 		__webpack_require__.h = function() { return "0bdfd15c95e7f357"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
